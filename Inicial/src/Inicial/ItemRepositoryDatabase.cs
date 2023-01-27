a6477ee9a61b7fe9cd6c690290f6c44ac40c8f11
tdr <tdr.evolution@gmail.com>
2023-01-27 14:40:52 -0300

    Revert "Ports and Adapters"
    This reverts commit 4225c52993d025f6179bb89269e2cf0084d8ac0a.

--- Inicial/src/Inicial/ItemRepositoryDatabase.cs
+++ Inicial/src/Inicial/ItemRepositoryDatabase.cs
@@ -29,6 +29,7 @@ namespace Inicial
                 ItemModel? itemModel = (itemModels.Any()) ? itemModels.FirstOrDefault() : null;
                 if (itemModel != null)
                 {
+                    int i = itemModel.id_item;
                     item = new Item(itemModel.id_item, itemModel.description, itemModel.price, new Dimension(itemModel.width, itemModel.height, itemModel.length, itemModel.weight));                    
                 }
                 await connection.CloseAsync();
